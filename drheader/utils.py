9df056e8e91ac8bd5b9437922fb29af2f8427a05
Javier Horrillo <javier.horrillobarrantes@santander.co.uk>
2020-03-23 16:00:01 +0000

    adding new --merge option to merge custom file on top of default headers rules set, linked to Santandersecurityresearch/DrHeader#73

--- drheader/utils.py
+++ drheader/utils.py
@@ -19,11 +19,14 @@ def _to_lower_dict(some_dict):
     return result
 
 
-def load_rules(rule_file=None):
+def load_rules(rule_file=None, merge=None):
     """
     Loads drheader ruleset. Will load local defaults unless overridden.
+    If merge flag is present, result file will be a merge between local defaults and custom file
     :param rule_file: file object of rules.
     :type rule_file: file
+    :param merge: flag indicating to merge file_rule with default rules
+    :type rule_file: boolean
     :return: drheader rules
     :rtype: dict
     """
@@ -31,8 +34,30 @@ def load_rules(rule_file=None):
     if rule_file:
         logging.debug('')
         rules = yaml.safe_load(rule_file.read())
+        if merge:
+            with open(os.path.join(os.path.dirname(__file__), 'rules.yml'), 'r') as f:
+                default_rules = yaml.safe_load(f.read())
+            rules = merge_rules(default_rules, rules)
     else:
         with open(os.path.join(os.path.dirname(__file__), 'rules.yml'), 'r') as f:
             rules = yaml.safe_load(f.read())
 
     return rules['Headers']
+
+
+def merge_rules(default_rules, custom_rules):
+    """
+    Mege both rule set. Rules defined in 'custom_rules', also present in 'default_rules', will be overriden.
+    If a new rule is present in custom_rules, not present in default_rules, it will be added.
+    :param default_rules: base file object of rules.
+    :type default_rules: dict
+    :param custom_rules: override file object of rules.
+    :type custom_rules: dict
+    :return: final rule
+    :rtype: dict
+    """
+
+    for rule in custom_rules['Headers']:
+        default_rules['Headers'][rule] = custom_rules['Headers'][rule]
+
+    return default_rules
