1cff971d23435d4d26800b2e39464e17ac4d25b2
Javier Horrillo <javier.horrillobarrantes@santander.co.uk>
2020-03-31 17:28:14 +0100

    update version, dependencies and adding unit tests for new function 'file_junit_report'

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/santandersecurityresearch/drheader',
-    version='1.2.0',
+    version='1.3.0',
     zip_safe=False,
 )
