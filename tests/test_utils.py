7bedc720c11618554df8aadd29bb14b866fec503
Javier Horrillo <javier.horrillobarrantes@santander.co.uk>
2020-04-01 09:16:01 +0100

    closing file in previous tests plus adding junit-xml as dev dependency

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -14,10 +14,13 @@ class TestUtilsFunctions(unittest2.TestCase):
     def setUp(self):
         with open(os.path.join(os.path.dirname(__file__), 'testfiles/default_rules.yml'), 'r') as f:
             self.default_rules = yaml.safe_load(f.read())
+            f.close()
         with open(os.path.join(os.path.dirname(__file__), 'testfiles/custom_rules.yml'), 'r') as f:
             self.custom_rules = yaml.safe_load(f.read())
+            f.close()
         with open(os.path.join(os.path.dirname(__file__), 'testfiles/custom_rules_merged.yml'), 'r') as f:
             self.custom_rules_merged = yaml.safe_load(f.read())
+            f.close()
 
     def test_load_rules_default(self):
         rules = load_rules()
@@ -26,12 +29,14 @@ class TestUtilsFunctions(unittest2.TestCase):
     def test_load_rules_custom(self):
         with open(os.path.join(os.path.dirname(__file__), 'testfiles/custom_rules.yml'), 'r') as f:
             rules = load_rules(f)
+            f.close()
         self.assertNotEqual(rules, self.default_rules['Headers'])
         self.assertEqual(rules, self.custom_rules['Headers'])
 
     def test_load_rules_custom_and_merge(self):
         with open(os.path.join(os.path.dirname(__file__), 'testfiles/custom_rules.yml'), 'r') as f:
             rules = load_rules(f, True)
+            f.close()
         self.assertNotEqual(rules, self.default_rules['Headers'])
         self.assertNotEqual(rules, self.custom_rules['Headers'])
         self.assertEqual(rules, self.custom_rules_merged['Headers'])
