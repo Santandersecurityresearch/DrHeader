7fd222ffa7fd22c63c2fe43949619dda1935f4e9
pealtrufo <32867433+pealtrufo@users.noreply.github.com>
2020-04-29 14:23:40 +0100

    no need for Pypi_description.md file

--- setup.py
+++ setup.py
@@ -6,12 +6,21 @@
 from setuptools import setup
 
 import os
+import re
 
 base_dir = os.path.dirname(__file__)
 
 
-with open(os.path.join(base_dir, "Pypi_description.md")) as f:
-    long_description = f.read()
+# with open(os.path.join(base_dir, "Pypi_description.md")) as f:
+#     long_description = f.read()
+
+long_description = ''
+
+with open(os.path.join(base_dir, "README.md")) as readme:
+    readme_lines = readme.readlines()
+    for line in readme_lines:
+        if not re.search(r'\(assets\/img\b',line):
+            long_description = long_description + line
 
 with open('HISTORY.md') as history_file:
     history = history_file.read()
