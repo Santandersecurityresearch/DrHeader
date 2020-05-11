b6aa1257b4a157268145bbf76f0b43ae85da4f86
GitHub Action <action@github.com>
2020-05-11 15:51:42 +0000

    Bump version: 1.2.1 → 1.2.2

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements, 
     url='https://github.com/santandersecurityresearch/drheader',
-    version='1.2.1',
+    version='1.2.2',
     zip_safe=False,
 )
