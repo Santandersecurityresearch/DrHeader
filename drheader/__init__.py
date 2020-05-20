d2095eff08d8d7bb7b7f05ff17e1ef610cda5de4
GitHub Action <action@github.com>
2020-05-20 15:31:53 +0000

    Bump version: 1.3.0 → 1.3.1

--- drheader/__init__.py
+++ drheader/__init__.py
@@ -2,6 +2,6 @@
 
 """Top-level package for drHEADer core."""
 
-__version__ = '1.3.0'
+__version__ = '1.3.1'
 
 from drheader.core import Drheader  # noqa
--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements, 
     url='https://github.com/santandersecurityresearch/drheader',
-    version='1.3.0',
+    version='1.3.1',
     zip_safe=False,
 )
