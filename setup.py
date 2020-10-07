557c93dabe778412920b40c55fa2ee4b678a3924
BodneyC <benjcarrington@gmail.com>
2020-10-07 21:08:02 +0100

    feat(reqs): Read reqs from file, JsonSchema@3.2.0 (#125)

--- setup.py
+++ setup.py
@@ -21,14 +21,8 @@ with open(os.path.join(base_dir, "README.md")) as readme:
 with open('HISTORY.md') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0',
-                'requests>=2.22.0',
-                'jsonschema==3.0.2',
-                'jsonschema[format]',
-                'validators>=0.14.0',
-                'tabulate==0.8.3',
-                'pyyaml==5.3.1',
-                'junit-xml==1.9']
+with open('requirements.txt') as f:
+    requirements = f.read()
 
 setup_requirements = ['pytest-runner', ]
 
